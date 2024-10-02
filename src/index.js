8bd9a318536b42a14f8d282e049282d449e14b55
Prank Shadow <prankshadow@gmail.com>
2024-10-02 16:41:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    DATABASE_URL: process.env.DATABASE_URL
+      ? process.env.DATABASE_URL
+      : "not_here",
+    DEBUG: process.env.DEBUG ? process.env.DEBUG : "not_debug",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+exports.handler = serverless(app);
